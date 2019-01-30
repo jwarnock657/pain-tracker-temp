7b2c9ebb662c682c5add0c077d6e7ed3bd63ec50
smoriya1 <2271595m@student.gla.ac.uk>
2019-01-30 11:39:25 +0000

    Pain Input layout changes

---
+++ PASPainTracker/main/src/test/java/teamsevendream/paspaintracker/main/PainInputUnitTest.java
@@ -0,0 +1,23 @@
+package teamsevendream.paspaintracker.main;
+
+import android.content.Context;
+import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PainInputUnitTest {
+
+    @Mock
+    Context mMockContext;
+
+    @Test
+    public void test(){
+        //something
+    }
+}
